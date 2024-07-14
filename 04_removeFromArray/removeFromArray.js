c173e062befd23d5ea1f11325b079cff34081807
Luong Tran <tranthihienluong2003@gmail.com>
2024-07-14 13:35:12 +0700

    Finish 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+  for (const value of values) {
+    let index = array.indexOf(value);
+    while (index > -1) {
+      array.splice(index, 1);
+      index = array.indexOf(value);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
