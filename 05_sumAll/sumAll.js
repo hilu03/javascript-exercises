bfea0265707f6311b4085d6c69d7704325356ee6
Luong Tran <tranthihienluong2003@gmail.com>
2024-07-14 13:48:26 +0700

    Finish 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if (typeof(num1) != "number" || typeof(num2) != "number") {
+    return "ERROR";
+  } 
+  else if (num1 <= 0 || num2 <= 0) {
+    return "ERROR";
+  }
+  let sum = 0;
+  let start = num1;
+  let end = num2;
+  if (num1 > num2) {
+    start = num2;
+    end = num1;
+  }
+  for (let i = start; i<=end; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
