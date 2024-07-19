9550f66f779507a72686b2c2afbbcf98f1d1bf43
Luong Tran <tranthihienluong2003@gmail.com>
2024-07-19 22:09:15 +0700

    Finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, i) => total + i, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((mul, i) => mul * i, 1);
 };
 
-const power = function() {
-	
+const power = function(num, p) {
+	let result = 1;
+  for (let i=0; i<p; i++) {
+    result *= num;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1;
+  }
+  let fa = 1;
+  for (let i=1; i<=num; i++) {
+    fa *= i;
+  }
+  return fa;
 };
 
 // Do not edit below this line
