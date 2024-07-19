e4a7f00052ade3ea5d451c2b6616fb3cb00a60f8
Luong Tran <tranthihienluong2003@gmail.com>
2024-07-19 22:44:40 +0700

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.toUpperCase();
+  let words = [];
+  for (let i = 0; i<str.length; i++) {
+    if (((str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) || (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) || (str.charCodeAt(i) >= 48 && str.charCodeAt(i) <= 57))) {
+      words.push(str.charCodeAt(i));
+    }
+  }
+  let reverse = (words.slice()).reverse();
+  return reverse.every((word, index) => word === words[index]);
 };
 
 // Do not edit below this line
