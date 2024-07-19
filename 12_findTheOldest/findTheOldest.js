ef3ac3e94387f7368571e372ce33a7d68d31f559
Luong Tran <tranthihienluong2003@gmail.com>
2024-07-19 23:35:04 +0700

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.reduce((oldest, person) => {
+    if (!person.yearOfDeath) {
+      person.age = new Date().getFullYear() - person.yearOfBirth;
+    }
+    else {
+      person.age = person.yearOfDeath - person.yearOfBirth;
+    }
+    if (!oldest.yearOfDeath) {
+      oldest.age = new Date().getFullYear() - oldest.yearOfBirth;
+    }
+    else {
+      oldest.age = oldest.yearOfDeath - oldest.yearOfBirth;
+    }
+    return oldest.age < person.age? person: oldest;
+  });
 };
 
 // Do not edit below this line
