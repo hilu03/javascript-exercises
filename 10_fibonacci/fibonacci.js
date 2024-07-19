53c3fb09e9c325fc09d2525048717966c3f1a96d
Luong Tran <tranthihienluong2003@gmail.com>
2024-07-19 22:55:25 +0700

    Finish 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  n = +n;
+  if (n < 0) {
+    return "OOPS";
+  }
+  if (n === 0) {
+    return 0;
+  }
+  if (n < 3) {
+    return 1;
+  }
+  let prev1 = 1;
+  let prev2 = 1;
+  let result;
+  for (let i=3; i<=n; i++) {
+    result = prev1 + prev2;
+    prev2 = prev1;
+    prev1 = result;
+  }
+  return result;
 };
 
 // Do not edit below this line
